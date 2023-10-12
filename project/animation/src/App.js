57c0bbfa4d8ec6054e13a43e2b7726b36005a3a2
Jess <145494578+Jessica-Isunza@users.noreply.github.com>
2023-10-11 18:34:06 -0600

    animation commit 1
    falta no repetir la animacion infinitamente y hacer display a nuevas bolitas de welcome cuando acabe la animación

---
+++ project/animation/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div class="ball">
+      <div class="ball-t1">W</div>
+      <div class="ball-t">E</div>
+      <div class="ball-t1">L</div>
+      <div class="ball-t">C</div>
+      <div class="ball-t1">O</div>
+      <div class="ball-t">M</div>
+      <div class="ball-t1">E</div>
+     </div> 
+    </div>
+  );
+}
+
+export default App;
